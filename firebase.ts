a8c12a5ff34eab2ff07d85376caa6c0bc51bdb37
victor <victorzemail@gmail.com>
2024-01-21 21:39:46 -0600

    remove 1 from env local

--- firebase.ts
+++ firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
-// TODO: Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
